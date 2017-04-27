667310a6e72abf572ac7e58582874dcd20f76922
Marlene Lomborg <mlombo16@student.aau.dk>
2017-04-27 22:38:43 +0200

    Ball moving slower and in like a tube

--- app/src/main/java/com/example/marlene/maze/MainActivity.java
+++ app/src/main/java/com/example/marlene/maze/MainActivity.java
@@ -8,6 +8,7 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.Display;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,9 +20,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private SensorManager senSensorManager;
     private Sensor senAccelerometer;
 
-    private long lastUpdate = 0;
-    private float last_x, last_y, last_z;
-    private static final int SHAKE_THRESHOLD = 600;
+    public float AccX, AccY = 0.0f;
+    public float PosX, PosY = 0.0f;
+    public float VelX, VelY = 0.0f;
+
+    public float frameTime = 0.666f;
+    public float xmax,ymax;
 
 
 
@@ -29,51 +33,67 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Display display = getWindowManager().getDefaultDisplay();
+        xmax = (float) display.getWidth() - 50;
+        ymax = (float) display.getHeight() - 50;
 
         senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
+
+
 
     }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         Sensor mySensor = sensorEvent.sensor;
-        TextView text = (TextView)findViewById(R.id.textView);
+
         TextView text2 = (TextView)findViewById(R.id.textView2);
         TextView text3 = (TextView)findViewById(R.id.textView3);
         ImageView img = (ImageView) findViewById(R.id.imageView1);
 
-        DisplayMetrics metrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(metrics);
 
         if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            float x = sensorEvent.values[0];
-            float y = sensorEvent.values[1];
-            float z = sensorEvent.values[2];
-            float tempX;
-            float tempY;
-            float tempz;
+            //sensor values(x and y) and current time.
+            AccX = sensorEvent.values[0];
+            AccY = sensorEvent.values[1];
+            long t = System.currentTimeMillis();
+
+            updateBall();
 
 
-            text.setText("Z:" + Float.toString(z));
-            text2.setText("X:" + Float.toString(x));
-            text3.setText("Y:" + Float.toString(y));
 
-            tempX = img.getTranslationX();
-            tempY = img.getTranslationY();
+            img.setX(PosX);
+            img.setY(PosY);
 
-
-            //Continue from the getTranslationX and Y above. We need to move the ball from it's existing location using values from -10 to 10. THen we need to Set an obstacle and make colliders. A victory state is needed as well.
-
-            last_x = x;
-            last_y = y;
-            last_z = z;
-
-
-            img.setX((metrics.widthPixels / 2 ) + Math.round((last_x))* -10);
-            img.setY((metrics.heightPixels / 2 ) + Math.round((last_y)) * 10);
         }
+    }
+
+    public void updateBall(){
+
+        VelX += (AccX * frameTime);
+        VelY += (AccY * frameTime);
+
+
+        float xS = (VelX/2)*frameTime;
+        float yS = (VelY/2)*frameTime;
+
+        PosX -= xS;
+        PosY -= yS;
+
+        if (PosX > xmax) {
+            PosX = xmax;
+        } else if (PosX < 0) {
+            PosX = 0;
+        }
+        if (PosY > ymax) {
+            PosY = ymax;
+        } else if (PosY < 0) {
+            PosY = 0;
+        }
+
+
     }
 
     @Override
